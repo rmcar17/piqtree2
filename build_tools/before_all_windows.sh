0286bab0c2d11c53ea4901add43ec66909acf14e
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-09-30 05:40:40 +0000

    DEV: add y flag to choco install

--- build_tools/before_all_windows.sh
+++ build_tools/before_all_windows.sh
@@ -1,5 +1,5 @@
-choco install boost-msvc-14.1
-choco install eigen
-choco install make
+choco install -y boost-msvc-14.1
+choco install -y eigen
+choco install -y make
 
 bash build_tools/build_iqtree.sh
