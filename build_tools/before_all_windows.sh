219df470ca69895909a1fc74326e3acc3a75f848
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-10-01 02:40:09 +0000

    DEV: update windows build ci

--- build_tools/before_all_windows.sh
+++ build_tools/before_all_windows.sh
@@ -1,5 +1,4 @@
-choco install -y boost-msvc-14.1 
-choco install -y llvm --version=14.0.6
+choco install -y llvm --version=14.0.6 --allow-downgrade
 choco install -y eigen
 choco install -y make
 
@@ -7,7 +6,7 @@ choco install -y make
 cd iqtree2
 rm -rf build
 mkdir build && cd build
-cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_MAKE_PROGRAM=make -DIQTREE_FLAGS="single" -DBUILD_LIB=ON ..
+cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS=--target=x86_64-pc-windows-gnu -DCMAKE_CXX_FLAGS=--target=x86_64-pc-windows-gnu -DCMAKE_MAKE_PROGRAM=mingw32-make -DIQTREE_FLAGS="single" -DBUILD_LIB=ON ..
 make -j
 cd ../..
 mv iqtree2/build/libiqtree2.a src/piqtree2/_libiqtree/
