eb0d48ba5b52602fd41371744251d191b03cb2be
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-10-01 01:08:54 +0000

    DEV: add windows specific cmake call

--- build_tools/before_all_windows.sh
+++ build_tools/before_all_windows.sh
@@ -3,4 +3,11 @@ choco install -y llvm --version=14.0.6
 choco install -y eigen
 choco install -y make
 
-bash build_tools/build_iqtree.sh
+# bash build_tools/build_iqtree.sh
+cd iqtree2
+rm -rf build
+mkdir build && cd build
+cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_MAKE_PROGRAM=make -DIQTREE_FLAGS="single" -DBUILD_LIB=ON ..
+make -j
+cd ../..
+mv iqtree2/build/libiqtree2.a src/piqtree2/_libiqtree/
