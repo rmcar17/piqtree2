918285f1c320d25eb7c0cd5c00464dbe971162fa
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-10-03 09:29:47 +0000

    DEV: revert zlib

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
-import platform
 
 LIBRARY_DIR = "src/piqtree2/_libiqtree"
 
@@ -11,7 +10,7 @@ ext_modules = [
         "_piqtree2",
         ["src/piqtree2/_libiqtree/_piqtree2.cpp"],
         library_dirs=[LIBRARY_DIR],
-        libraries=["iqtree2", "z" if platform.platform() != "Windows" else "zlib"],
+        libraries=["iqtree2", "z"],
     ),
 ]
 
