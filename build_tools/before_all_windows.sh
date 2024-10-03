dcf352a65e018e826a0443fb65ba6dc8882ed43a
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-10-03 07:25:58 +0000

    DEV: fully allow building on win/mac/linux

--- build_tools/before_all_windows.sh
+++ build_tools/before_all_windows.sh
@@ -4,10 +4,4 @@ echo $RUNNER_OS
 choco install -y llvm --version=14.0.6 --allow-downgrade
 choco install -y eigen
 
-cd iqtree2
-rm -rf build
-mkdir build && cd build
-cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS=--target=x86_64-pc-windows-gnu -DCMAKE_CXX_FLAGS=--target=x86_64-pc-windows-gnu -DCMAKE_MAKE_PROGRAM=mingw32-make -DIQTREE_FLAGS="single" -DBUILD_LIB=ON ..
-make -j
-cd ../..
-mv iqtree2/build/libiqtree2.a src/piqtree2/_libiqtree/
+bash build_tools/build_iqtree.sh
