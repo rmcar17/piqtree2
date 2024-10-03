6fe273cee06ea4dbb1756ddad322203d8f7b72bc
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-10-03 08:11:08 +0000

    DEV: rename to lib format on windows

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 """setup for piqtree2."""
 
+import pathlib
+import platform
+
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
 LIBRARY_DIR = "src/piqtree2/_libiqtree"
+
+if platform.system() == "Windows":
+    src = pathlib.Path(LIBRARY_DIR) / "libiqtree2.a"
+    dest = pathlib.Path(LIBRARY_DIR) / "iqtree.lib"
+    if src.exists():
+        src.rename(dest)
 
 ext_modules = [
     Pybind11Extension(
