6973842f9497e00b587ac509263e268430d91d16
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-07 10:38:50 +1100

    DEV: try using vcpkg version of zlib before building iq-tree

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import platform
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-LIBRARY_DIR = "src\\piqtree2\\_libiqtree"
+LIBRARY_DIR = "src/piqtree2/_libiqtree"
 
 ext_modules = [
     Pybind11Extension(
         "_piqtree2",
-        ["src\\piqtree2\\_libiqtree\\_piqtree2.cpp"],
-        include_dirs=["vcpkg\\installed\\x64-windows-static\\include", "iqtree2\\build", "iqtree2", "iqtree2\\yaml-cpp\\include"],
-        library_dirs=[LIBRARY_DIR, "vcpkg\\installed\\x64-windows-static\\lib"],
+        ["src/piqtree2/_libiqtree/_piqtree2.cpp"],
+        include_dirs=["vcpkg/installed/x64-windows-static/include"],
+        library_dirs=[LIBRARY_DIR, "vcpkg/installed/x64-windows-static/lib"],
         libraries=["iqtree2", "zlib" if platform.system() == "Windows" else "z"],
     ),
 ]
