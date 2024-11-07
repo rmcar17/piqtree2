eb9fdf0f97518de7bc2834ad4d7350fd8336a4a6
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-07 12:11:19 +1100

    DEV: add back DZLIB flags

--- build_tools/build_iqtree.sh
+++ build_tools/build_iqtree.sh
@@ -19,10 +19,10 @@ if [[ "$RUNNER_OS" == "Windows" ]]; then
     -DBoost_INCLUDE_DIR='$BOOST_ROOT/include' \
     -DBoost_LIBRARY_DIRS='$BOOST_ROOT/lib' \
     -DCMAKE_TOOLCHAIN_FILE='$VCPKG_ROOT\\scripts\\buildsystems\\vcpkg.cmake' \
+    -DZLIB_INCLUDE_DIR='$ZLIB_ROOT\\include' \
+    -DZLIB_LIBRARY='$ZLIB_ROOT\\lib' \
     -DIQTREE_FLAGS='single' -DBUILD_LIB=ON .."
 fi
-    # -DZLIB_INCLUDE_DIR='$ZLIB_ROOT\\include' \
-    # -DZLIB_LIBRARY='$ZLIB_ROOT\\lib' \
 
 eval $cmake_cmd
 
