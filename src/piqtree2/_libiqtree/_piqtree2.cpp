807d48e32fc8181de3a92936263aea9249aa0085
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-15 12:02:56 +1100

    DEV: include new files in diff, try to export as c functions

--- src/piqtree2/_libiqtree/_piqtree2.cpp
+++ src/piqtree2/_libiqtree/_piqtree2.cpp
@@ -5,6 +5,12 @@
 #include <string>
 #include <vector>
 
+extern "C" {
+    // Declaration of the new C wrapper function
+    const char* build_tree(const char** names, const char** seqs, int names_size, int seqs_size, const char* model, int rand_seed);
+    void free_result(const char* result);
+}
+
 using namespace std;
 
 namespace py = pybind11;
@@ -22,10 +28,33 @@ extern string random_tree(int num_taxa,
 
 // Perform phylogenetic analysis on the input alignment (in string format)
 // With estimation of the best topology
-extern string build_tree(vector<string>& names,
-                         vector<string>& seqs,
-                         string model,
-                         int rand_seed);
+std::string iq_build_tree(const std::vector<std::string>& names,
+                           const std::vector<std::string>& seqs,
+                           const std::string& model,
+                           int rand_seed) {
+    // Convert the input vectors to C-style arrays for C wrapper
+    const char* names_arr[names.size()];
+    const char* seqs_arr[seqs.size()];
+
+    for (size_t i = 0; i < names.size(); ++i) {
+        names_arr[i] = names[i].c_str();
+    }
+
+    for (size_t i = 0; i < seqs.size(); ++i) {
+        seqs_arr[i] = seqs[i].c_str();
+    }
+
+    // Call the C function
+    const char* result = build_tree(names_arr, seqs_arr, names.size(), seqs.size(), model.c_str(), rand_seed);
+
+    // Make sure to copy the C-string result to a std::string (as Python expects a std::string)
+    std::string result_str(result);
+
+    // Free the allocated result string
+    free_result(result);
+
+    return result_str;
+}
 
 // Perform phylogenetic analysis on the input alignment (in string format)
 // With restriction to the input toplogy
@@ -48,7 +77,7 @@ PYBIND11_MODULE(_piqtree2, m) {
         "Generates a set of random phylogenetic trees. tree_gen_mode "
         "allows:\"YULE_HARDING\", \"UNIFORM\", \"CATERPILLAR\", \"BALANCED\", "
         "\"BIRTH_DEATH\", \"STAR_TREE\".");
-  m.def("iq_build_tree", &build_tree,
+  m.def("iq_build_tree", &iq_build_tree,
         "Perform phylogenetic analysis on the input alignment (in string "
         "format). With estimation of the best topology.");
   m.def("iq_fit_tree", &fit_tree,
