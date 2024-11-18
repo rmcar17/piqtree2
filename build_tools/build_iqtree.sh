39598d791d28a67483d1b4bcacf2df92dbe21a47
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-18 05:55:31 +0000

    DEV: allow for multithreading support

--- build_tools/build_iqtree.sh
+++ build_tools/build_iqtree.sh
@@ -1,7 +1,8 @@
 cd iqtree2
+git apply ../openmp.patch
 rm -rf build
 mkdir build && cd build
-cmake -DIQTREE_FLAGS="single" -DBUILD_LIB=ON ..
+cmake -DBUILD_LIB=ON ..
 make -j
 cd ../..
 mv iqtree2/build/libiqtree2.a src/piqtree2/_libiqtree/
