74430a221ac2d6f38f283215b270e5a04339a435
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-19 03:11:55 +0000

    DEV: try adding homebrew to cppflags and ldflags

--- build_tools/before_all_mac.sh
+++ build_tools/before_all_mac.sh
@@ -1,4 +1,12 @@
 brew update
 brew install eigen boost gcc libomp
 
+echo "Here"
+echo $LDFLAGS
+echo $CPPFLAGS
+echo "There"
+
+export LDFLAGS="-L/opt/homebrew/opt/libomp/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/libomp/include"
+
 bash build_tools/build_iqtree.sh
