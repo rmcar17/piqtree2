dbed8e49b2fbdcce9dc8bf087dab258a30d97417
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-22 05:49:32 +0000

    DEV: add LLVM include and lib

--- build_tools/before_all_mac.sh
+++ build_tools/before_all_mac.sh
@@ -4,11 +4,13 @@ brew install eigen boost llvm gcc libomp cmake
 LLVM_BIN=$(brew --prefix llvm)/bin
 OMP_LIB=$(brew --prefix libomp)/lib
 OMP_INCLUDE=$(brew --prefix libomp)/include
+LLVM_LIB=$(brew --prefix llvm)/lib
+LLVM_INCLUDE=$(brew --prefix libomp)/include
 
 export CC=$LLVM_BIN/clang
 export CXX=$LLVM_BIN/clang++
-export LDFLAGS="-L$OMP_LIB"
-export CPPFLAGS="-I$OMP_INCLUDE"
+export LDFLAGS="-L$OMP_LIB -L$LLVM_LIB"
+export CPPFLAGS="-I$OMP_INCLUDE -I$LLVM_INCLUDE"
 export OpenMP_C_FLAGS="-fopenmp -L$OMP_LIB -I$OMP_INCLUDE"
 export OpenMP_CXX_FLAGS="-fopenmp -L$OMP_LIB -I$OMP_INCLUDE"
 export OpenMP_C_LIB_NAMES="libomp"
