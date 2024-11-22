d159ce61eb8964e785d353fd8cfc45d9a5d78d4c
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-22 05:00:57 +0000

    DEV: remove xclang flag

--- build_tools/before_all_mac.sh
+++ build_tools/before_all_mac.sh
@@ -10,7 +10,7 @@ export CC=$GCC_BIN
 export CXX=$GPP_BIN
 export LDFLAGS="-L$OMP_LIB"
 export CPPFLAGS="-I$OMP_INCLUDE"
-export OPENMP_C_FLAGS="-Xclang -fopenmp -L$OMP_LIB -I$OMP_INCLUDE"
+export OPENMP_C_FLAGS="-fopenmp -L$OMP_LIB -I$OMP_INCLUDE"
 export OPENMP_CXX_FLAGS="-Xclang -fopenmp -L$OMP_LIB -I$OMP_INCLUDE"
 
 bash build_tools/build_iqtree.sh
