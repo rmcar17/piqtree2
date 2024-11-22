dbad6e9f88ac51ba45d9dd7c83d922e702fb84f3
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-22 07:09:39 +0000

    DEV: change compiler depending on OS

--- build_tools/build_iqtree.sh
+++ build_tools/build_iqtree.sh
@@ -3,7 +3,20 @@ rm -rf build
 mkdir build && cd build
 echo $CC
 echo $CXX
-cmake -DBUILD_LIB=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ..
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    C_COMPILER="clang"
+    CXX_COMPILER="clang++"
+else
+    C_COMPILER="gcc"
+    CXX_COMPILER="g++"
+fi
+
+cmake -DBUILD_LIB=ON \
+      -DCMAKE_C_COMPILER="$C_COMPILER" \
+      -DCMAKE_CXX_COMPILER="$CXX_COMPILER" \
+      ..
+
 make -j
 cd ../..
 mv iqtree2/build/libiqtree2.a src/piqtree2/_libiqtree/
