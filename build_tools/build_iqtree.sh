bc274b1cff1fcd6e2f14aac3e98d6bdb24803a11
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-11-22 05:05:25 +0000

    DEV: rename c flags

--- build_tools/build_iqtree.sh
+++ build_tools/build_iqtree.sh
@@ -1,7 +1,7 @@
 cd iqtree2
 rm -rf build
 mkdir build && cd build
-cmake -DBUILD_LIB=ON -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_FLAGS="$OPENMP_C_FLAGS" -DCMAKE_CXX_FLAGS="$OPENMP_CXX_FLAGS" ..
+cmake -DBUILD_LIB=ON -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_FLAGS="$OpenMP_C_FLAGS" -DCMAKE_CXX_FLAGS="$OPENMP_CXX_FLAGS" ..
 make -j
 cd ../..
 mv iqtree2/build/libiqtree2.a src/piqtree2/_libiqtree/
